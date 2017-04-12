29ecb3814570ff4c1bf7a83ef9cecfbb0972b7f9
panchenkoroman <r.panchenko21@gmail.com>
2017-04-12 16:02:28 +0300

    many ditail fixed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 window.onload = function () {
     var modal = document.getElementById('modal'),
-        modalBtn = document.getElementById('modalBtn');
+        modalBtn = document.getElementById('modalBtn'),
+        close = document.getElementById('closeX');
 
     modalBtn.onclick = function () {
         modal.style.display = 'block';
+        modalBtn.classList.remove('header-insert');
+        modalBtn.classList.add('disabled');
     }
 
     window.onclick = function (event) {
-        if (event.target == modal) {
+        if (event.target == modal || event.target == close) {
             modal.style.display = 'none';
         }
     }
-}
 
+    var primary = document.getElementById('btn-prime');
+
+    primary.onclick = function (e) {
+        if (e.target == primary) {
+            primary.classList.add('primary-disabled');
+        }
+    }
+
+}
 
