a3d9b20c9046be057674207047678e0fe7510f88
panchenkoroman <r.panchenko21@gmail.com>
2017-04-14 13:46:15 +0300

    select is done

--- js/select.js
+++ js/select.js
@@ -1,34 +1,25 @@
 /**
  * Created by RomanPC on 14.04.2017.
  */
-// Iterate over each select element
 $('select').not('#money').not('#sort').each(function () {
 
-    // Cache the number of options
     var $this = $(this),
         numberOfOptions = $(this).children('option').length;
 
-    // Hides the select element
     $this.addClass('s-hidden');
 
-    // Wrap the select element in a div
     $this.wrap('<div class="select2"></div>');
 
-    // Insert a styled div to sit over the top of the hidden select element
     $this.after('<div class="styledSelect"></div>');
 
-    // Cache the styled div
     var $styledSelect = $this.next('div.styledSelect');
 
-    // Show the first select option in the styled div
     $styledSelect.text($this.children('option').eq(0).text());
 
-    // Insert an unordered list after the styled div and also cache the list
     var $list = $('<ul />', {
         'class': 'options'
     }).insertAfter($styledSelect);
 
-    // Insert a list item into the unordered list for each select option
     for (var i = 0; i < numberOfOptions; i++) {
         $('<li />', {
             text: $this.children('option').eq(i).text(),
@@ -36,10 +27,8 @@ $('select').not('#money').not('#sort').each(function () {
         }).appendTo($list);
     }
 
-    // Cache the list items
     var $listItems = $list.children('li');
 
-    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
     $styledSelect.click(function (e) {
         e.stopPropagation();
         $('div.styledSelect.active').each(function () {
@@ -48,17 +37,14 @@ $('select').not('#money').not('#sort').each(function () {
         $(this).toggleClass('active').next('ul.options').toggle();
     });
 
-    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
-    // Updates the select element to have the value of the equivalent option
+
     $listItems.click(function (e) {
         e.stopPropagation();
         $styledSelect.text($(this).text()).removeClass('active');
         $this.val($(this).attr('rel'));
         $list.hide();
-        /* alert($this.val()); Uncomment this for demonstration! */
     });
 
-    // Hides the unordered list when clicking outside of it
     $(document).click(function () {
         $styledSelect.removeClass('active');
         $list.hide();
